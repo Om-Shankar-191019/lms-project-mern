53c7d25fcd49487012b49a760e9e84fe0ce1c822
Om-Shankar-191019 <om8003golu@gmail.com>
2025-07-18 06:57:19 +0530

    fix: update API URLs to use environment variables for production

--- server/controllers/coursePurchase.controller.js
+++ server/controllers/coursePurchase.controller.js
@@ -39,8 +39,10 @@ export const createCheckoutSession = async (req, res) => {
         },
       ],
       mode: "payment",
-      success_url: `http://localhost:5173/course-progress/${courseId}`, // once payment successful redirect to course progress page
-      cancel_url: `http://localhost:5173/course-detail/${courseId}`,
+      // success_url: `http://localhost:5173/course-progress/${courseId}`, // once payment successful redirect to course progress page
+      // cancel_url: `http://localhost:5173/course-detail/${courseId}`,
+      success_url: `https://lms-project-mern.onrender.com/course-progress/${courseId}`, // once payment successful redirect to course progress page
+      cancel_url: `https://lms-project-mern.onrender.com/course-detail/${courseId}`,
       metadata: {
         courseId: courseId,
         userId: userId,
